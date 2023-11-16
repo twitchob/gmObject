b473936d10f146068577f4df8f7ae54eaf5af733
张忠源 <zhangzhongyuan@szanfu.cn>
2023-11-16 09:06:29 +0800

    feat(公钥私钥格式转换):新增公钥私钥工具类,后续逐步完善各种格式转换

--- src/main/java/com/zzy/utils/PublicKeyUtils.java
+++ src/main/java/com/zzy/utils/PublicKeyUtils.java
@@ -24,32 +24,96 @@ public class PublicKeyUtils {
         Security.addProvider(new BouncyCastleProvider());
     }
 
-    /**
-     * ECC 公钥转  BCECPublicKey
-     */
-    public static BCECPublicKey eccToBCECPublicKey(String publicKey) throws Exception {
-        byte[] decode = Base64.decode(publicKey);
-        BigInteger x = BigIntegerUtil.toPositiveInteger(ArrayUtil.sub(decode, 1, 33));
-        BigInteger y = BigIntegerUtil.toPositiveInteger(ArrayUtil.sub(decode, 33, 65));
-        return getBCECPublicKeyFromXY(x, y);
-    }
 
     /**
-     * BCECPublicKey 转 ECC公钥
+     * ECC 获取x y
+     *
+     * @param publicKey Base64编码的 ECC格式公钥(04+32字节x+32字节Y)
+     * @return Base64编码的 32字节x+32字节Y
      */
-    public static String bcecPublicKeyToECC(BCECPublicKey publicKey) {
-        org.bouncycastle.math.ec.ECPoint q = publicKey.getQ();
-        byte[] x = q.getXCoord().getEncoded();
-        byte[] y = q.getYCoord().getEncoded();
-        byte[] bytes = ArrayUtil.addAll(new byte[]{0x04}, x, y);
+    public static String getXYFromECC(String publicKey) {
+        byte[] decode = Base64.decode(publicKey);
+        byte[] x = ArrayUtil.sub(decode, 1, 33);
+        byte[] y = ArrayUtil.sub(decode, 33, 65);
+        byte[] bytes = ArrayUtil.addAll(x, y);
         return Base64.encode(bytes);
     }
 
     /**
-     * 通过x y 获取BCECPublicKey
+     * X Y 获取ECC
      *
-     * @param x 32字节
-     * @param y 32字节
+     * @param xy Base64编码的 32字节x+32字节Y
+     * @return Base64编码的 ECC格式公钥(04+32字节x+32字节Y)
+     */
+    public static String getECCFromXY(String xy) {
+        byte[] decode = Base64.decode(xy);
+        byte[] x = ArrayUtil.sub(decode, 0, 32);
+        byte[] y = ArrayUtil.sub(decode, 32, 64);
+        byte[] bytes = ArrayUtil.addAll(new byte[]{0x04}, x, y);
+        return Base64.encode(bytes);
+    }
+
+
+    /**
+     * 0018 获取x y
+     *
+     * @param publicKey Base64编码的 0018格式公钥(4字节(256)+32字节X+32字节Y)
+     * @return Base64编码的 32字节x+32字节Y
+     */
+    public static String getXYFrom0018(String publicKey) {
+        byte[] decode = Base64.decode(publicKey);
+        byte[] x = ArrayUtil.sub(decode, 4, 36);
+        byte[] y = ArrayUtil.sub(decode, 36, 68);
+        byte[] bytes = ArrayUtil.addAll(x, y);
+        return Base64.encode(bytes);
+    }
+
+
+    /**
+     * X Y 获取0018
+     *
+     * @param xy Base64编码的 32字节x+32字节Y
+     * @return Base64编码的 0018格式公钥(4字节(256)+32字节X+32字节Y)
+     */
+    public static String get0018FromXY(String xy) {
+        byte[] decode = Base64.decode(xy);
+        byte[] x = ArrayUtil.sub(decode, 0, 32);
+        byte[] y = ArrayUtil.sub(decode, 32, 64);
+        byte[] bytes = ArrayUtil.addAll(new byte[]{0x00, 0x01, 0x00, 0x00}, x, y);
+        return Base64.encode(bytes);
+    }
+
+
+    /**
+     * BCECPublicKey 获取X Y
+     * @param publicKey BCECPublicKey
+     * @return Base64编码的 32字节x+32字节Y
+     */
+    public static String getXYFromBCECPublicKey(BCECPublicKey publicKey) {
+        org.bouncycastle.math.ec.ECPoint q = publicKey.getQ();
+        byte[] x = q.getXCoord().getEncoded();
+        byte[] y = q.getYCoord().getEncoded();
+        byte[] bytes = ArrayUtil.addAll(x, y);
+        return Base64.encode(bytes);
+    }
+
+    /**
+     * X Y 获取BCECPublicKey
+     * @param xy Base64编码的 32字节x+32字节Y
+     * @return BCECPublicKey
+     */
+    public static BCECPublicKey getBCECPublicKeyFromXY(String xy) throws Exception {
+        byte[] decode = Base64.decode(xy);
+        BigInteger x = BigIntegerUtil.toPositiveInteger(ArrayUtil.sub(decode, 0, 32));
+        BigInteger y = BigIntegerUtil.toPositiveInteger(ArrayUtil.sub(decode, 32, 64));
+        return getBCECPublicKeyFromXY(x, y);
+    }
+
+    /**
+     * 通过 x y 获取BCECPublicKey
+     *
+     * @param x BigInteger
+     * @param y BigInteger
      */
     public static BCECPublicKey getBCECPublicKeyFromXY(BigInteger x, BigInteger y) throws Exception {
         X9ECParameters x9ECParameters = GMNamedCurves.getByName("sm2p256v1");
@@ -58,6 +122,5 @@ public class PublicKeyUtils {
         KeyFactory keyFactory = KeyFactory.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
         return (BCECPublicKey) keyFactory.generatePublic(ecPublicKeySpec);
     }
-
 
 }
